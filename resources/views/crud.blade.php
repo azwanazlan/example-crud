6360ddc2561c34eb4e07713b6ddc26694883db5e
azwanazlan <azwanazlan@gmail.com>
2022-08-22 07:21:00 +0800

    first commit

---
+++ resources/views/crud.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel example-CRUD</title>
+<!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset">
+                <form action="" method="">
+
+                    <div class="form-group mt-4">
+                        <label for="">Name</label>
+                        <input type="text" class="form-control" name="name" placeholder="Enter Name">
+                    </div>
+
+                    <div class="form-group mt-4">
+                        <label for="">Favourite Color</label>
+                        <input type="text" class="form-control" name="favouriteColor" placeholder="Enter Favourite Color">
+                    </div>
+
+                    <div class="form-group mt-4">
+                        <label for="">Area</label>
+                        <input type="text" class="form-control" name="area" placeholder="Enter Area">
+                    </div>
+
+                    <div class="form-group mt-4">
+                        <button type="submit" class="btn btn-primary btn-block">SAVE</button>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
